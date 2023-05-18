8b5e5eedd517ddf94194cacef34b021091350603
Diego <maciasg.diego17@gmail.com>
2023-05-18 09:40:26 -0500

    Create script.js
    Javascript for the game

---
+++ script.js
@@ -0,0 +1,9 @@
+// This is a simple JavaScript script template.
+
+// Define the function that will be executed when the page loads.
+function myFunction() {
+  // Do something when the page loads.
+}
+
+// Add the function to the window.onload event.
+window.onload = myFunction;
