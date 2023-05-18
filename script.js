b2a06e73ab6efcaf0b0a683105827e1698667879
Diego <maciasg.diego17@gmail.com>
2023-05-18 14:07:08 -0500

    Added player and computer variables. Added getComputerChoice function. Added playRound function.

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-// This is a simple JavaScript script template.
+// This is a simple JavaScript rock, paper, scissors game. Authored by: Diego MG
 
-// Define the function that will be executed when the page loads.
-function myFunction() {
-  // Do something when the page loads.
+const pSelection = String(prompt("Rock, Paper or Scissors?"));
+console.log("Player has selected: " + pSelection)
+
+const cSelection = getComputerChoice();
+
+// A function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getComputerChoice() {
+  let randomVariable = "";
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      randomVariable = "rock";
+      break;
+    case 1:
+      randomVariable = "paper";
+      break;
+    case 2:
+      randomVariable = "scissors";
+      break;
+  }
+  return randomVariable;
+}
+console.log ("Bot has selected: " + cSelection);
+
+// Write a function that plays a single round of Rock Paper Scissors.
+function playRound(playerSelection, computerSelection) {
+  let lowercasePlayerSelection = playerSelection.toLowerCase();
+  if (lowercasePlayerSelection === "rock" && computerSelection === "paper") {
+    return ("You lose! Paper beats Rock.");
+  }
+  else if (lowercasePlayerSelection === "rock" && computerSelection === "scissors") {
+    return ("You win! Rock beats Scissors.");
+  }
+  else if (lowercasePlayerSelection === "paper" && computerSelection === "scissors") {
+    return ("You lose! Scissors beats Paper.");
+  }
+  else if (lowercasePlayerSelection === "paper" && computerSelection === "rock") {
+    return ("You win! Paper beats Rock.");
+  }
+  else if (lowercasePlayerSelection === "scissors" && computerSelection === "rock") {
+    return ("You lose! Rock beats Scissors.");
+  }
+  else if (lowercasePlayerSelection === "scissors" && computerSelection === "paper") {
+    return ("You win! Scissors beats Paper.");
+  }
+  else if (lowercasePlayerSelection === computerSelection) {
+    return ("Tie! Nobody wins nor loses.")
+  }
+  else {
+    return ("Welp, this is awkward!");
+  }
 }
 
-// Add the function to the window.onload event.
-window.onload = myFunction;
+let playRoundResult = playRound(pSelection, cSelection);
+console.log(playRoundResult);
+
+function game() {
+
+}
