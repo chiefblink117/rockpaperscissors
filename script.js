166dbc34f6b0ffe2a126f81529e60f30718393de
Diego <maciasg.diego17@gmail.com>
2023-05-19 11:05:00 -0500

    Added game() function. Added paragraph to index.html

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // This is a simple JavaScript rock, paper, scissors game. Authored by: Diego MG
 
-const pSelection = String(prompt("Rock, Paper or Scissors?"));
-console.log("Player has selected: " + pSelection)
-
-const cSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+game();
 
 // A function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
@@ -22,27 +21,32 @@ function getComputerChoice() {
   }
   return randomVariable;
 }
-console.log ("Bot has selected: " + cSelection);
 
-// Write a function that plays a single round of Rock Paper Scissors.
+// A function that plays a single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) {
   let lowercasePlayerSelection = playerSelection.toLowerCase();
   if (lowercasePlayerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     return ("You lose! Paper beats Rock.");
   }
   else if (lowercasePlayerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
     return ("You win! Rock beats Scissors.");
   }
   else if (lowercasePlayerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     return ("You lose! Scissors beats Paper.");
   }
   else if (lowercasePlayerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
     return ("You win! Paper beats Rock.");
   }
   else if (lowercasePlayerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     return ("You lose! Rock beats Scissors.");
   }
   else if (lowercasePlayerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return ("You win! Scissors beats Paper.");
   }
   else if (lowercasePlayerSelection === computerSelection) {
@@ -53,9 +57,25 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-let playRoundResult = playRound(pSelection, cSelection);
-console.log(playRoundResult);
-
+// A function that plays 5 rounds. It keeps score and reports a winner or loser at the end.
 function game() {
+  for (i=0; i < 5; i++) {
+    const pSelection = String(prompt("Rock, Paper or Scissors?"));
+    console.log("Player has selected: " + pSelection)
 
+    const cSelection = getComputerChoice();
+    console.log ("Bot has selected: " + cSelection);
+
+    let playRoundResult = playRound(pSelection, cSelection);
+    console.log(playRoundResult);
+    console.log("Score: " + "Player " + playerScore + " Bot " + computerScore)
+  }
+  console.log("Game Over")
+  if (playerScore === computerScore) {
+    console.log ("Final score: " + "Player " + playerScore + " Bot " + computerScore + " | It is a tie!")
+  } else if (playerScore > computerScore) {
+    console.log ("Final score: " + "Player " + playerScore + " Bot " + computerScore + " | Player Wins!")
+  } else {
+    console.log ("Final score: " + "Player " + playerScore + " Bot " + computerScore + " | Bot Wins!")
+  }
 }
