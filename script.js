29187a9781bfe1d9bc16940d2ac84a83744df291
Diego <maciasg.diego17@gmail.com>
2024-02-29 20:37:23 -0600

    new rock, paper and scissors version with some ui

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 
 let playerScore = 0;
 let computerScore = 0;
+
+const scoreContainer = document.querySelector('#gameScore');
+const scoreDisplay = document.createElement('p');
+scoreDisplay.textContent = "Score: " + "Player " + playerScore + " Bot " + computerScore;
+scoreContainer.appendChild(scoreDisplay);
+// Add the score interface to the page
+document.body.appendChild(scoreContainer);
+// Create a container for the game interface
+const gameContainer = document.querySelector('#Container');
+// Add the game interface to the page
+document.body.appendChild(gameContainer);
+
 game();
 
 // A function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
@@ -10,13 +22,13 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3)
   switch (randomNumber) {
     case 0:
-      randomVariable = "rock";
+      randomVariable = "Rock";
       break;
     case 1:
-      randomVariable = "paper";
+      randomVariable = "Paper";
       break;
     case 2:
-      randomVariable = "scissors";
+      randomVariable = "Scissors";
       break;
   }
   return randomVariable;
@@ -25,6 +37,7 @@ function getComputerChoice() {
 // A function that plays a single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
   if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
     return ("You lose! Paper beats Rock.");
@@ -59,23 +72,93 @@ function playRound(playerSelection, computerSelection) {
 
 // A function that plays 5 rounds. It keeps score and reports a winner or loser at the end.
 function game() {
-  for (i=0; i < 5; i++) {
-    const pSelection = String(prompt("Rock, Paper or Scissors?"));
-    console.log("Player has selected: " + pSelection)
 
-    const cSelection = getComputerChoice();
-    console.log ("Bot has selected: " + cSelection);
+  let pSelection = '';
+  let cSelection = '';
+  
+  // Create buttons
+  const rockButton = document.createElement('button');
+  rockButton.textContent = 'Rock';
+  gameContainer.appendChild(rockButton);
+  
+  const paperButton = document.createElement('button');
+  paperButton.textContent = 'Paper';
+  gameContainer.appendChild(paperButton);
 
-    let playRoundResult = playRound(pSelection, cSelection);
-    console.log(playRoundResult);
-    console.log("Score: " + "Player " + playerScore + " Bot " + computerScore)
-  }
-  console.log("Game Over")
-  if (playerScore === computerScore) {
-    console.log ("Final score: " + "Player " + playerScore + " Bot " + computerScore + " | It is a tie!")
-  } else if (playerScore > computerScore) {
-    console.log ("Final score: " + "Player " + playerScore + " Bot " + computerScore + " | Player Wins!")
-  } else {
-    console.log ("Final score: " + "Player " + playerScore + " Bot " + computerScore + " | Bot Wins!")
-  }
+  const scissorsButton = document.createElement('button');
+  scissorsButton.textContent = 'Scissors';
+  gameContainer.appendChild(scissorsButton);
+
+  // Create Display Elements
+  const pSelectionDisplay = document.createElement('p');
+  gameContainer.appendChild(pSelectionDisplay);
+  
+  const cSelectionDisplay = document.createElement('p');
+  gameContainer.appendChild(cSelectionDisplay);
+
+  const pResultDisplay = document.createElement('p');
+  gameContainer.appendChild(pResultDisplay);
+
+    // Add event listeners
+    rockButton.addEventListener('click', () => {
+      pSelection = 'Rock';
+      cSelection = getComputerChoice();
+
+      pSelectionDisplay.textContent = "Player has selected: " + pSelection;
+      cSelectionDisplay.textContent = "Bot has selected: " + cSelection;
+      pResultDisplay.textContent = playRound(pSelection, cSelection);
+      scoreDisplay.textContent = "Score: " + "Player " + playerScore + " Bot " + computerScore;
+
+      // Check for Winner
+      if (playerScore >= 5) {
+        declareWinner('Player');
+      } else if (computerScore >= 5) {
+        declareWinner('Bot'); 
+      }
+    });
+
+    paperButton.addEventListener('click', () => {
+      pSelection = 'Paper';
+      cSelection = getComputerChoice();
+
+      pSelectionDisplay.textContent = "Player has selected: " + pSelection;
+      cSelectionDisplay.textContent = "Bot has selected: " + cSelection;
+      pResultDisplay.textContent = playRound(pSelection, cSelection);
+      scoreDisplay.textContent = "Score: " + "Player " + playerScore + " Bot " + computerScore;
+
+      // Check for Winner
+      if (playerScore >= 5) {
+        declareWinner('Player');
+      } else if (computerScore >= 5) {
+        declareWinner('Bot'); 
+      }
+    });
+
+    scissorsButton.addEventListener('click', () => {
+      pSelection = 'Scissors';
+      cSelection = getComputerChoice();
+
+      pSelectionDisplay.textContent = "Player has selected: " + pSelection;
+      cSelectionDisplay.textContent = "Bot has selected: " + cSelection;
+      pResultDisplay.textContent = playRound(pSelection, cSelection);
+      scoreDisplay.textContent = "Score: " + "Player " + playerScore + " Bot " + computerScore;
+
+      // Check for Winner
+      if (playerScore >= 5) {
+        declareWinner('Player');
+      } else if (computerScore >= 5) {
+        declareWinner('Bot'); 
+      }
+     });
+}
+
+function declareWinner (winner) {
+
+  const end = document.createElement('p');
+  end.textContent = "Game Over";
+  gameContainer.appendChild(end);
+
+  const finalResult = document.createElement('p');
+  finalResult.textContent = "Final score: " + "Player " + playerScore + " - " + " Bot " + computerScore + " | " + winner + " Wins!";
+  gameContainer.appendChild(finalResult);
 }
